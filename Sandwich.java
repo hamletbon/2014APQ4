835e62ede6235f9186dd38383d528bf5768fd990
hamletbon <halin@hkis.edu.hk>
2016-09-20 13:59:36 +0800

    Sandwich class and MenuItem Interface
    Sandwich class and MenuItem Interface

---
+++ Sandwich.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Sandwich here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Sandwich implements MenuItem
+{
+    //fields
+    private String name;
+    private double price;
+    
+    //methods
+    public String getName(){
+        return name;
+    }
+    
+    public double getPrice(){
+        return price;
+    }
+}
